840cacba493cadcfe3bb1b9e0e85ed0f85110919
Victor Prado <61568003+victorpraado1005@users.noreply.github.com>
2022-05-20 17:43:08 -0300

    Ajustando função criarAlunos e mostrarAlunos

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 var turma = []
 
-function criarAluno(){
-  var name = document.getElementById('nameStudent').value
-  var lastName = document.getElementById('lastNameStudent').value
+function verificaDados(){
+  var nome = document.getElementById('nameStudent').value
+  var sobrenome = document.getElementById('lastNameStudent').value
   var n1 = document.getElementById('n1').value
   var n2 = document.getElementById('n2').value
+  n1 = parseFloat(n1);
+  n2 = parseFloat(n2);
 
-  var aluno = {
-    name : name,
-    lastName : lastName,
-    n1 : n1,
-    n2 : n2,
-    media : function(){
-      return (n1 + n2) / 2;
-    }
+  var a = new aluno(nome, sobrenome, n1, n2)
+  return turma.push(a)
+}
+
+function aluno(nome, sobrenome, n1, n2){
+  this.name = nome;
+  this.lastName = sobrenome;
+  this.n1 = n1;
+  this.n2 = n2;
+  this.media = function(){
+     var resultado = (n1 + n2) / 2;
+     return resultado;
   }
-  return turma.push(aluno)
+}
+
+function mostrarAlunos(){
+  let tbody = document.getElementById('tbody')
+  tbody.innerText = '';
+
+  for (let i = 0; i < turma.length; i++) {
+    let tr = tbody.insertRow();
+
+    let td_nome = tr.insertCell();
+    let td_sobrenome = tr.insertCell();
+    let td_n1 = tr.insertCell();
+    let td_n2 = tr.insertCell();
+
+    td_nome.innerText = this.turma[i].name;
+    td_sobrenome.innerText = this.turma[i].lastName;
+    td_n1.innerText = this.turma[i].n1;
+    td_n2.innerText = this.turma[i].n2;
+  }
+}
+
+function esconderAlunos(){
+  let tbody = document.getElementById('tbody')
+  tbody.innerText = '';
 }
