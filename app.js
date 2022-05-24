0a2d13cc6dd864516f5e17e2ad229f3f381f9f91
Victor Prado <prado.victor1005@gmail.com>
2022-05-23 22:40:28 -0300

    Feature de selecionar turma criada, e funcao de exibir por turma finalizada

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var turma = []
+var alunos = []
 
 function verificaDados(){
   var nome = document.getElementById('nameStudent').value
@@ -7,16 +7,23 @@ function verificaDados(){
   var n2 = document.getElementById('n2').value
   n1 = parseFloat(n1);
   n2 = parseFloat(n2);
-
-  var a = new aluno(nome, sobrenome, n1, n2)
-  return turma.push(a)
+  var turma = getClass()
+  var a = new aluno(nome, sobrenome, n1, n2, turma)
+  clearInput()
+  return alunos.push(a)  
 }
 
-function aluno(nome, sobrenome, n1, n2){
+function getClass(){
+  var select = document.getElementById('turma')
+  return select.options[select.selectedIndex].value
+}
+
+function aluno(nome, sobrenome, n1, n2, turma){
   this.name = nome;
   this.lastName = sobrenome;
   this.n1 = n1;
   this.n2 = n2;
+  this.turma = turma;
   this.media = function(){
      var resultado = (n1 + n2) / 2;
      return resultado;
@@ -26,23 +33,40 @@ function aluno(nome, sobrenome, n1, n2){
 function mostrarAlunos(){
   let tbody = document.getElementById('tbody')
   tbody.innerText = '';
+  var select = document.getElementById('turmaVisualizar')
+  var turmaVisualizar = select.options[select.selectedIndex].value
 
-  for (let i = 0; i < turma.length; i++) {
-    let tr = tbody.insertRow();
+  for (let i = 0; i < alunos.length; i++) {
+    if(alunos[i].turma == turmaVisualizar){
+      let tr = tbody.insertRow();
 
-    let td_nome = tr.insertCell();
-    let td_sobrenome = tr.insertCell();
-    let td_n1 = tr.insertCell();
-    let td_n2 = tr.insertCell();
+      let td_nome = tr.insertCell();
+      let td_sobrenome = tr.insertCell();
+      let td_n1 = tr.insertCell();
+      let td_n2 = tr.insertCell();
+      let td_media = tr.insertCell();
 
-    td_nome.innerText = this.turma[i].name;
-    td_sobrenome.innerText = this.turma[i].lastName;
-    td_n1.innerText = this.turma[i].n1;
-    td_n2.innerText = this.turma[i].n2;
+      td_nome.innerText = this.alunos[i].name;
+      td_sobrenome.innerText = this.alunos[i].lastName;
+      td_n1.innerText = this.alunos[i].n1;
+      td_n2.innerText = this.alunos[i].n2;
+      td_media.innerText = this.alunos[i].media();
+    }
   }
-}
+  }
 
-function esconderAlunos(){
-  let tbody = document.getElementById('tbody')
-  tbody.innerText = '';
-}
+  function verificaTamanhoTurma(turma){
+    
+  }
+
+  function clearInput(){
+    document.getElementById('nameStudent').value='';
+    document.getElementById('lastNameStudent').value='';
+    document.getElementById('n1').value='';
+    document.getElementById('n2').value='';
+  }
+
+  function hideStudents(){
+    let tbody = document.getElementById('tbody')
+    tbody.innerText = '';
+  }
