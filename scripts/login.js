03d935a97176849c417645e1f3b8ca590475cd20
Victor Prado <prado.victor1005@gmail.com>
2022-05-29 22:42:36 -0300

    Criação e estilização da página de Login. App conectado ao Firebase. Login funcionando.

---
+++ scripts/login.js
@@ -0,0 +1,39 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBrFXiPOBMwNTGR9mhKicqk-8dPOleHTNk",
+  authDomain: "schooljs-d5a0c.firebaseapp.com",
+  projectId: "schooljs-d5a0c",
+  storageBucket: "schooljs-d5a0c.appspot.com",
+  messagingSenderId: "700833576926",
+  appId: "1:700833576926:web:404cd59ac38650e0258f4f"
+};
+
+firebase.initializeApp(firebaseConfig)
+const auth = firebase.auth()
+
+function createAccount(email, password){
+  var email = document.getElementById('createEmail').value
+  var password = document.getElementById('createPassword').value
+
+  auth.createUserWithEmailAndPassword(email, password)
+  .then(userC=>{
+    var user = userC.user
+    console.log(user)
+  }).catch(error=>{
+    console.log(error)
+  })
+}
+
+function login(){
+  var email = document.getElementById('email').value
+  var password = document.getElementById('password').value
+
+  auth.signInWithEmailAndPassword(email, password)
+  .then(userL=>{
+    var user = userL.user
+    console.log(userL)
+    window.location.href = "pages/home.html";
+  }).catch(error=>{
+    alert('Usuário ou senha incorreta. Tente novamente!')
+    console.log(error)
+  })
+}
